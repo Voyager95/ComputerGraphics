b3b07f4789f22287c6009e46fe8df892f194da91
Voyager95 <41989566+Voyager95@users.noreply.github.com>
2020-09-03 03:04:49 +0900

    워밍업 문제 1 완료

--- WarmingUp1/WarmingUp1/WarmingUp1.cpp
+++ WarmingUp1/WarmingUp1/WarmingUp1.cpp
@@ -5,18 +5,44 @@
 #include <cstdlib>
 #include <cstdio>
 #include <ctime>
-#define LENG_M3 3
-#define LENG_M4 4
+#include <math.h>
 using namespace std;
 
-class Matrix
+class SquareMatrix
 {
 private:
     int m_Size;
+
+    int GetDeterminant(SquareMatrix& s)
+    {
+        int result = 0;
+
+        if (s.GetSize() > 2)
+        {
+            for (int i = 0; i < s.GetSize(); ++i)
+            {
+                SquareMatrix* excepted = s.RowColumnExceptedMatrix(0, i);
+                result += s.value[0][i] * GetDeterminant( *excepted ) * (int)pow(-1, i);
+
+                delete excepted;
+            }
+        }
+        else if(s.GetSize() == 2)
+        {
+            result = s.value[0][0] * s.value[1][1] - s.value[0][1] * s.value[1][0];
+        }
+        else
+        {
+            cout << "행렬 크기에 문제가 있습니다" << endl;
+        }
+
+        return result;
+    }
+
 public:
     int** value;
 
-    Matrix(int size)
+    SquareMatrix(int size)
     {
         this->m_Size = size;
         value = new int*[size];
@@ -28,7 +54,7 @@ public:
         InitializeValue();
     }
 
-    ~Matrix()
+    ~SquareMatrix()
     {
         for (int i = 0; i < m_Size; ++i)
         {
@@ -38,15 +64,13 @@ public:
         delete[] value;
     }
 
-    int GetSize()
+    int GetSize() const
     {
         return m_Size;
     }
 
     void InitializeValue()
     {
-        srand((unsigned int)time(NULL));
-
         for (int r = 0; r < m_Size; ++r)
         {
             for (int c = 0; c < m_Size; ++c)
@@ -61,15 +85,12 @@ public:
 
     void ChangeRandomValue()
     {
-        srand((unsigned int)time(NULL));
 
         for (int r = 0; r < m_Size; ++r)
         {
             for (int c = 0; c < m_Size; ++c)
             {
                 int randValue = rand();
-
-                //cout << randValue << endl;
 
                 value[r][c]= randValue % 2;
             }
@@ -89,75 +110,221 @@ public:
         }
     }
 
-    void Copy(Matrix target)
+    void Copy(const SquareMatrix& target)
     {
         int targetSize = target.GetSize();
 
         if (targetSize > m_Size)
             targetSize = m_Size;
 
-        for (int r = 0; r < m_Size; ++r)
+        for (int r = 0; r < targetSize; ++r)
         {
-            for (int c = 0; c < m_Size; ++c)
+            for (int c = 0; c < targetSize; ++c)
             {
                 value[r][c] = target.value[r][c];
             }
         }
     }
 
-    Matrix operator*(Matrix m)
+    SquareMatrix* RowColumnExceptedMatrix(int rowToExcept, int columnToExcept)
     {
-        Matrix result = Matrix(m.GetSize());
+        int size = GetSize() - 1;
+        SquareMatrix* result = new SquareMatrix(size);
+
+        if (size < 2)
+        {
+            cout << "사이즈가 너무 작습니다" << endl;
+            return result;
+        }
+
+
+        for (int r = 0; r < m_Size; ++r)
+        {
+            if (rowToExcept != r)
+            {
+                for (int c = 0; c < m_Size; ++c)
+                {
+                    if (columnToExcept != c)
+                    {
+                        int rowCorrectionValue = 0;
+                        int columnCorrectionValue = 0;
+
+                        if (r > rowToExcept)
+                            rowCorrectionValue -= 1;
+
+                        if (c > columnToExcept)
+                            columnCorrectionValue -= 1;
+
+                        result->value[r + rowCorrectionValue][c + columnCorrectionValue] = value[r][c];
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    int GetDeterminant()
+    {
+        return GetDeterminant(*this);
+    }
+
+    SquareMatrix* GetTransposedMatrix()
+    {
+        SquareMatrix* result = new SquareMatrix(GetSize());
 
         for (int r = 0; r < m_Size; ++r)
         {
             for (int c = 0; c < m_Size; ++c)
             {
-                int result = 0;
-                for (int i = 0; i < m_Size; ++i)
-                {
-                    result += value[r][i] * m.value[i][c];
-                }
-
-                value[r][c] = result;
+                result->value[r][c] = value[c][r];
             }
         }
+
+        return result;
     }
+
+
+    SquareMatrix* operator*(const SquareMatrix& m)
+    {
+        SquareMatrix* result = new SquareMatrix(m.GetSize());
+
+        for (int r = 0; r < m_Size; ++r)
+        {
+            for (int c = 0; c < m_Size; ++c)
+            {
+                int element = 0;
+                for (int i = 0; i < m_Size; ++i)
+                {
+                    element += value[r][i] * m.value[i][c];
+                }
+
+                result->value[r][c] = element;
+            }
+        }
+
+        return result;
+    }
+
+    SquareMatrix* operator+(const SquareMatrix& m)
+    {
+        SquareMatrix* result = new SquareMatrix(m.GetSize());
+
+        for (int r = 0; r < m_Size; ++r)
+        {
+            for (int c = 0; c < m_Size; ++c)
+            {
+                result->value[r][c] = value[r][c] + m.value[r][c];
+            }
+        }
+
+        return result;
+    }
+
+    SquareMatrix* operator-(const SquareMatrix& m)
+    {
+        SquareMatrix* result = new SquareMatrix(m.GetSize());
+
+        for (int r = 0; r < m_Size; ++r)
+        {
+            for (int c = 0; c < m_Size; ++c)
+            {
+                result->value[r][c] = value[r][c] - m.value[r][c];
+            }
+        }
+
+        return result;
+    }
+
 };
 
 int main()
 {
-    Matrix A = Matrix(3);
-    Matrix B = Matrix(3);
+    srand((unsigned int)time(NULL));
+
+    SquareMatrix* A = new SquareMatrix(3);
+    A->ChangeRandomValue();
+    SquareMatrix* B = new SquareMatrix(3);
+    B->ChangeRandomValue();
+
     while(true)
     {
         system("cls");
         // 안내
         cout << "A = " << endl;
-        A.Show();
+        A->Show();
         cout << "B = " << endl;
-        B.Show();
-        cout << " - m: 행렬의 곱셈 \n - a : 행렬의 덧셈 \n - d : 행렬의 뺄셈 \n - r : 행렬식의 값(Determinant) \n - t : 전치 행렬(Transposed matrix)과 그 행렬식의 값" <<
+        B->Show();
+        cout << " - m : 행렬의 곱셈 \n - a : 행렬의 덧셈 \n - d : 행렬의 뺄셈 \n - r : 행렬식의 값(Determinant) \n - t : 전치 행렬(Transposed matrix)과 그 행렬식의 값" <<
         "\n - h : 3X3 행렬을 4X4 행렬로 변환하고 행렬식의 값(4행4열 행렬식 값) 출력  \n - s : 행렬의 값을 새로 랜덤하게 설정한다. \n - q : 프로그램 종료" << endl;
-        
-        fflush(stdin);
-        char code = getchar();
+
+        char code = getchar(); getchar();
         
         switch (code)
         {
         case 'm':
-            break;
+        {
+            SquareMatrix* m = (*A * *B);
+            m->Show();
+
+            delete m;
+        }
+        break;
         case 'a':
-            break;
+        {
+            SquareMatrix* a = (*A + *B);
+            a->Show();
+
+            delete a;
+        }
+        break;
         case 'd':
-            break;
+        {
+            SquareMatrix* d = (*A - *B);
+            d->Show();
+
+            delete d;
+        }
+        break;
         case 'r':
+            cout << "Det(A) = " << A->GetDeterminant() << endl;
+            cout << "Det(B) = " << A->GetDeterminant() << endl;
             break;
         case 't':
+        {
+            SquareMatrix* t1 = A->GetTransposedMatrix();
+            SquareMatrix* t2 = B->GetTransposedMatrix();
+            cout << "T(A) = " << endl;
+            t1->Show();
+            cout << "Det(T(A)) = " << t1->GetDeterminant() << endl;
+            cout << "T(B) = " << endl;
+            t2->Show();
+            cout << "Det(T(B)) = " << t2->GetDeterminant() << endl;
+            delete t1, t2;
+        }
             break;
         case 'h':
-            break;
+        {
+            SquareMatrix* h1 = new SquareMatrix(4);
+            SquareMatrix* h2 = new SquareMatrix(4);
+            h1->Copy(*A);
+            h2->Copy(*B);
+
+            cout << "A의 4X4 = " << endl;
+            h1->Show();
+            cout << "Det(A의 4X4) = " << h1->GetDeterminant() << endl;
+
+            cout << "B의 4X4 = " << endl;
+            h2->Show();
+            cout << "Det(B의 4X4) = " << h2->GetDeterminant() << endl;
+
+            delete h1, h2;
+        }
+        break;
         case 's':
+            A->ChangeRandomValue();
+            B->ChangeRandomValue();
+            cout << "바뀌었습니다" << endl;
             break;
         case 'q':
             exit(1);
@@ -166,8 +333,8 @@ int main()
             cout << "명령어를 다시 입력해주세요" << endl;
             break;
         }
-        fflush(stdin);
-        getchar();
+
+        system("PAUSE");
     }
 
 
